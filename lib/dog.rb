48387c3a93d961bbc68701e89cc799cb931dd58c
Erica Little <eteney90@gmail.com>
2021-03-24 22:35:23 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(dog_name)
+    @pup_name = dog_name
+  end
+
+  def name
+    @pup_name
+  end
+
+  def breed=(dog_breed)
+    @pup_breed = dog_breed
+  end
+
+  def breed
+    @pup_breed
+  end
+end
